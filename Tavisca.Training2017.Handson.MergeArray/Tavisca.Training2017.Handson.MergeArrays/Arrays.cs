8142a3f943bbe9a0ac69c48ed2adbb3519ee6ccf
Prabhash <pmohanty@tavisca.com>
2017-07-10 18:08:30 +0530

    File Added

---
+++ Tavisca.Training2017.Handson.MergeArray/Tavisca.Training2017.Handson.MergeArrays/Arrays.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Training2017.Handson.MergeArrays
+{
+    public class Arrays
+    {
+        internal int []arr;
+        public Arrays(int n)
+        {
+            this.arr = new int[n];
+        }
+       internal void Merge(int[] array, int left, int mid, int right)
+        {
+            int i, j, k;
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+
+            int[] leftArray = new int[n1];
+            int[] rightArray = new int[n2];
+
+
+            for (i = 0; i < n1; i++)
+                leftArray[i] = array[left + i];
+            for (j = 0; j < n2; j++)
+                rightArray[j] = array[mid + 1 + j];
+
+
+            i = 0;
+            j = 0;
+            k = left;
+            while (i < n1 && j < n2)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+
+        internal int[] MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+
+                int mid = left + (right - left) / 2;
+                MergeSort(array, left, mid);
+                MergeSort(array, mid + 1, right);
+
+                Merge(array, left, mid, right);
+                //return array;
+            }
+            return array;
+        }
+        internal void Print()
+        {
+            
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
---
+++ Tavisca.Training2017.Handson.MergeArray/Tavisca.Training2017.Handson.MergeArrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Tavisca.Training2017.Handson.MergeArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            Console.WriteLine("Enter the number of arrays");
+            int numArrays = Convert.ToInt32(Console.ReadLine());
+            Arrays []a = new Arrays[numArrays];
+            Arrays[] result2 = new Arrays[numArrays];
+            for (int i = 0; i < numArrays; i++)
+            {
+                Console.WriteLine("Enter the dimension of Array {0} ", i);
+                int num = Convert.ToInt32(Console.ReadLine());
+                a[i] = new Arrays(num);
+                result2[i] = new Arrays(num);
+
+            }
+            for (int i = 0; i < numArrays - 1; i++)
+            {
+                length = length + a[i].arr.Length + a[i + 1].arr.Length;
+            }
+            Arrays result = new Arrays(length);
+            for (int i = 0; i < numArrays; i++)
+            { 
+                Console.WriteLine("Enter the elements for Array {0}", i);
+                for(int j=0;j<a[i].arr.Length;j++)
+                {
+                    a[i].arr[j] = Convert.ToInt32(Console.ReadLine());
+                }
+                
+            }
+            
+            int count = 0;
+            for(int i=0;i<numArrays;i++)
+            {
+                for (int j=0;j<a[i].arr.Length;j++)
+                {
+                    result.arr[count++] = a[i].arr[j];
+                }
+            }
+            result.MergeSort(result.arr, 0, result.arr.Length-1);
+            Console.WriteLine("Sorted array is");
+            result.Print();
+            Console.ReadLine();  
+        }
+    }
+}
