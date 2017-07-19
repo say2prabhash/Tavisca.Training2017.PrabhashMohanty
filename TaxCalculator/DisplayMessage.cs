95073bfe4ce637e229764d8845a3163d0e8f2118
say2prabhash <pmohanty@tavisca.com>
2017-07-19 17:47:16 +0530

    Add files via upload

---
+++ TaxCalculator/DisplayMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    class DisplayMessage
+    {
+        public void DisplayCalculatedTax(double tax)
+        {
+            Console.WriteLine("Total tax calculated = " + tax);
+        }
+    }
+}
---
+++ TaxCalculator/Input.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    class Input
+    {
+        private double amount;
+        public void InputAmount()
+        {
+            Console.WriteLine("Enter the amount to be taxed");
+            double.TryParse(Console.ReadLine(), out amount);
+        }
+        public double GetAmount()
+        {
+            return amount;
+        }
+    }
+}
